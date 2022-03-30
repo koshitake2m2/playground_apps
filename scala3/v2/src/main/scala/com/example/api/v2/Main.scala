9c755d4ea97a25e7aa5c2e54a735943da0e09ce7
koshitake2m2 <blueling2m2dvp@gmail.com>
2022-03-30 14:34:37 +0900

    add scala3

---
+++ scala3/v2/src/main/scala/com/example/api/v2/Main.scala
@@ -0,0 +1,12 @@
+package com.example.api.v2
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+    println(msg)
+  }
+
+  def msg = "I was compiled by Scala 3. :)"
+
+}
